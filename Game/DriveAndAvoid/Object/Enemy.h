b21743481610b7e1c84eb796a874c49db588321f
tokumotoshotaro001 <138739547+tokumotoshotaro001@users.noreply.github.com>
2024-01-23 11:12:25 +0900

    Vector2D書いてる

--- Game/DriveAndAvoid/Object/Enemy.h
+++ Game/DriveAndAvoid/Object/Enemy.h
@@ -1,5 +1,26 @@
 #pragma once
+#include "../Utlity/Vector2D.h"
+
 class Enemy
 {
+private:
+	int type;	//タイプ
+	int image;	//画像
+	float speed;	//速さ
+	Vector2D location;	//位置情報
+	Vector2D box_size;	//当たり判定の大きさ
+
+public:
+	Enemy(int type, int handle);
+	~Enemy();
+
+	void Initialize();
+	void Update(float speed);
+	void Draw() const;
+	void Finalize();
+
+	int GetType() const;
+	Vector2D GetLocation() const;
+	Vector2D GetBoxSize() const;
 };
 
--- Game/DriveAndAvoid/Utility/Vector2D.h
+++ Game/DriveAndAvoid/Utility/Vector2D.h
@@ -1,1 +1,12 @@
 #pragma once
+
+//2次元ベクトルクラス
+class Vector2D
+{
+public:
+	float x;
+	float y;
+
+public:
+	Vector2D():
+};
