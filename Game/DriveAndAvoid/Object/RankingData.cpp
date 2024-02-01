4818648b9c02a323621209dfd3a15eed038e9397
tokumotoshotaro001 <138739547+tokumotoshotaro001@users.noreply.github.com>
2024-02-01 21:57:28 +0900

    a

--- Game/DriveAndAvoid/Object/RankingData.cpp
+++ Game/DriveAndAvoid/Object/RankingData.cpp
@@ -40,6 +40,14 @@ void RankingData::Initialize()
 	{
 		fscanf_s(fp, "%6d,%2d,%[^,],\n", &score[i], &rank[i], name[i], 15);
 	}
+
+	//ファイルクローズ
+	fclose(fp);
+
+	//末尾データの設定
+	score[5] = 0;
+	rank[5] = 0;
+	name[5][0] = '\0';
 }
 
 //終了処理
@@ -73,4 +81,64 @@ int RankingData::GetRank(int value) const
 const char* RankingData::GetName(int value) const
 {
 	return name[value];
+}
+
+//データ入れ替え処理
+void RankingData::SortData()
+{
+	//選択法ソートを使用し、降順で入れ替える
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = i+1; j < 6; j++)
+		{
+			if (score[i] <= score[j])
+			{
+				int tmp = score[i];
+				score[i] = score[j];
+				score[j] = tmp;
+
+				char buf[15] = {};
+				strcpy_s(buf, name[i]);
+				strcpy_s(name[i], name[j]);
+				strcpy_s(name[j], buf);
+			}
+		}
+	}
+
+	//順位を整列させる
+	for (int i = 0; i < 5; i++)
+	{
+		rank[i] = 1;
+	}
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = i+1; j < 5; j++)
+		{
+			if (score[i] > score[j])
+			{
+				rank[j]++;
+			}
+		}
+	}
+
+	//ランキングデータの書き込み
+	FILE* fp = nullptr;
+
+	//ファイルオープン
+	errno_t result = fopen_s(&fp, "Resource/dat/ranking_data.csv", "w");
+
+	//エラーチェック
+	if (result != 0)
+	{
+		throw("Resource/ranking_data.csvが開けませんでした\n");
+	}
+
+	//対象ファイルに書き込む
+	for (int i = 0; i < 5; i++)
+	{
+		fprintf(fp, "%d,%d,%s\n", score[i], rank[i], name[i]);
+	}
+
+	//ファイルクローズ
+	fclose(fp);
 }
