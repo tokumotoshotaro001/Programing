7374539082349a9a5662e245e74755ba8c114849
tokumotoshotaro001 <138739547+tokumotoshotaro001@users.noreply.github.com>
2024-02-06 15:02:18 +0900

    a

--- Game/DriveAndAvoid/Scene/ResultScene.cpp
+++ Game/DriveAndAvoid/Scene/ResultScene.cpp
@@ -1,1 +1,36 @@
 #include "ResultScene.h"
+#include "../Object/RankingData.h"
+#include "../Utility/InputControl.h"
+#include "DxLib.h"
+
+ResultScene::ResultScene() : back_ground(NULL), score(0)
+{
+	for (int i = 0; i < 3; i++)
+	{
+		enemy_image[i] = NULL;
+		enemy_count[i] = NULL;
+	}
+}
+
+ResultScene::~ResultScene()
+{
+
+}
+
+//初期化処理
+void ResultScene::Initialize()
+{
+	//画像の読み込み
+	back_ground = LoadGraph("Resource/images/back.bmp");
+	int result = LoadDivGraph("Resource/images/car.bmp", 3, 3, 1, 63, 120, enemy_image);
+
+	//エラーチェック
+	if (back_ground == -1)
+	{
+		throw("Resource/imageimages/back.bmgがありません\n");
+	}
+	if (result == -1)
+	{
+		throw("Resource/imageimages/car.bmgがありません\n");
+	}
+}
--- Game/DriveAndAvoid/Scene/TitleScene.cpp
+++ Game/DriveAndAvoid/Scene/TitleScene.cpp
@@ -41,6 +41,68 @@ eSceneType TitleScene::Update()
 	//カーソル下移動
 	if (InputControl::GetButtonDown(XINPUT_BUTTON_DPAD_DOWN))
 	{
-
+		menu_cursor++;
+		//１番下にしたら、一番上にする
+		if (menu_cursor > 3)
+		{
+			menu_cursor = 0;
+		}
 	}
+
+	//カーソル上移動
+	if (InputControl::GetButtonDown(XINPUT_BUTTON_DPAD_UP))
+	{
+		menu_cursor--;
+		//1番上に到達したら、一番下にする
+		if (menu_cursor < 0)
+		{
+			menu_cursor = 3;
+		}
+	}
+
+	//カーソル決定（決定した画面に遷移する）
+	if (InputControl::GetButtonDown(XINPUT_BUTTON_B))
+	{
+		switch (menu_cursor)
+		{
+		case 0:
+			return eSceneType::E_MAIN;
+		case 1:
+			return eSceneType::E_RANKING_DISP;
+		case 2:
+			return eSceneType::E_HELP;
+		default:
+			return eSceneType::E_END;
+		}
+	}
+
+	//現在のシーンタイプを返す
+	return GetNowScene();
+}
+
+//描画処理
+void TitleScene::Draw() const
+{
+	//タイトル画面の描画
+	DrawGraph(0, 0, background_image, FALSE);
+
+	//メニュー画面の描画
+	DrawGraph(120, 200, menu_image, TRUE);
+
+	//カーソル画面の描画
+	DrawRotaGraph(90, 220 + menu_cursor * 40, 0.7, DX_PI / 2.0, cursor_image, TRUE);
+}
+
+void TitleScene::Finalize()
+{
+	//読み込んだ画像の削除
+	DeleteGraph(background_image);
+	DeleteGraph(menu_image);
+	DeleteGraph(cursor_image);
+}
+
+//現在のシーン情報を取得
+eSceneType TitleScene::GetNowScene() const
+{
+	return eSceneType::E_TITLE;
 }
